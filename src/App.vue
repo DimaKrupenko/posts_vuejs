7ed6e33ebacb71453bbbb5c36082f23a193bd27c
DimaKrupenko <dimakrupenko77@gmail.com>
2023-07-07 15:55:38 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,32 @@
     <div class="post">
       <div><strong>Название:</strong>Пост о JavaScript</div>
       <div><strong>Описание:</strong>JavaScript универсальный язык програмирования</div>
-      </div>
+    </div>
   </div>
 
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'Javascript', body: 'Описание поста'}
+      ]
+    }
+  }
+}
 </script>
 
-<style scoped> 
-
+<style> 
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
 </style>
