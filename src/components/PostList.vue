1fe34a562681284c20f6e0d14faacccc7423145e
DimaKrupenko <dimakrupenko77@gmail.com>
2023-07-07 19:02:21 +0300

    upd

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="post" v-for="post in posts">
-        <div><strong>Название:</strong> {{ post.title }}</div>
-        <div><strong>Описание:</strong> {{ post.body }}</div>
+    <div>
+        <h3>Список пользователей</h3>
+        <post-item  
+        v-for="post in posts"
+        :post="post"
+        />
     </div>
+      
 </template>
 
 <script>
-    export default {
+import PostItem from '@/components/PostItem'
+export default {
+        components: {PostItem},
     props: {
         posts: {
             type: Array,
