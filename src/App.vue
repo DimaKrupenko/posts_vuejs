8c19cd8a1216fe601deca644f07b90a5d98fe040
DimaKrupenko <dimakrupenko77@gmail.com>
2023-07-13 16:23:58 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,17 @@
    v-if="!isPostsLoading"
    />
    <div v-else>Идет загрузка</div>
+   <div class="page__wrapper">
+     <div 
+     v-for="pageNumber in totalPage"
+      :key="pageNumber"
+      class="page"
+      :class="{
+        'current-page': page === pageNumber
+      }"
+      @click="changePage(pageNumber)"
+      >{{ pageNumber }}</div>
+   </div>
   </div>
 </template>
 
@@ -47,6 +58,9 @@ export default {
       isPostsLoading: false,
       selectedSort: '',
       searchQuery: '',
+      page: 1,
+      limit: 10,
+      totalPage: 0,
       sortOptions: [
         { value: 'title', name: 'По названию' },
         { value: 'body', name: 'По содержимому' },
@@ -64,10 +78,20 @@ export default {
     showDialog() {
      this.dialogVisible = true
     },
+    changePage(pageNumber) {
+      this.page = pageNumber
+    },
    async fetchPosts() {
      try { 
       this.isPostsLoading = true
-      const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10')
+       const response = await axios.get('https://jsonplaceholder.typicode.com/posts',
+         {
+           params: {
+             _page: this.page,
+             _limit: this.limit
+           }
+         })
+      this.totalPage = Math.ceil(response.headers['x-total-count'] / this.limit)
        this.posts = response.data
      } catch (e) {
        alert('ошибка')
@@ -108,6 +132,16 @@ margin: 15px 0;
 display: flex;
 justify-content: space-between;
 }
-
+.page__wrapper {
+display: flex;
+margin-top: 15px;
+}
+.page {
+  border: 1px solid black;
+  padding: 10px;
+}
+.current-page {
+border: 2px solid teal;
+}
 
 </style>
