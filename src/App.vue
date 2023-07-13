cb4f9f65f6c99ad3283c184378f5df1f4bbe4004
DimaKrupenko <dimakrupenko77@gmail.com>
2023-07-13 15:56:02 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
+    <my-input
+    v-model="searchQuery"
+    placeholder="Поиск ..."
+    />
     <div class="app__btns">
       <my-button @click="showDialog">
         Создать пост
@@ -18,7 +22,7 @@
     </my-dialog>
   
    <post-list 
-   :posts="sortedPosts"
+   :posts="sortedAndSearchedPosts"
    @remove="removePost"
    v-if="!isPostsLoading"
    />
@@ -42,6 +46,7 @@ export default {
       dialogVisible: false,
       isPostsLoading: false,
       selectedSort: '',
+      searchQuery: '',
       sortOptions: [
         { value: 'title', name: 'По названию' },
         { value: 'body', name: 'По содержимому' },
@@ -77,6 +82,9 @@ export default {
   computed: {
     sortedPosts() {
   return [...this.posts].sort((post1, post2) => post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]))
+    },
+    sortedAndSearchedPosts() {
+  return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
 }
   },
   
